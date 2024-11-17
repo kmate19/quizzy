426e148aca0724a7a8b928d002ff52d9755242bb
kmate19 <kissmate019@gmail.com>
2024-11-17 18:38:30 +0100

    Basic dockerfiles created

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,16 +1,20 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+type asd = {
+    name: string;
+}
 
-const port = 3000
-console.log(`Server is running on http://localhost:${port}`)
+app.get("/", (c) => {
+    return c.text("Hello Hono!");
+});
+
+const port = 3000;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
-  fetch: app.fetch,
-  port
-})
+    fetch: app.fetch,
+    port,
+});
--- apps/websocket/src/index.ts
+++ apps/websocket/src/index.ts
@@ -1,16 +1,16 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-    return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-const port = 3001
-console.log(`Server is running on http://localhost:${port}`)
+const port = 3001;
+console.log(`Server is running on http://localhost:${port}`);
 
 serve({
-    fetch: app.fetch,
-    port
-})
+  fetch: app.fetch,
+  port,
+});
