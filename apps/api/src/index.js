7f65f2e01a4dca9513f933f881a87bc703c56f77
kmate19 <kissmate019@gmail.com>
2024-11-17 20:00:31 +0100

    huu

--- apps/api/src/index.js
+++ apps/api/src/index.js
@@ -1,18 +1,11 @@
 import { serve } from "@hono/node-server";
-import { add } from "@repo/connect";
 import { Hono } from "hono";
-
 const app = new Hono();
-
 app.get("/", (c) => {
     return c.text("Hello Hono!");
 });
-
-const port = 3001;
+const port = 3000;
 console.log(`Server is running on http://localhost:${port}`);
-
-console.log(add(1, 2));
-
 serve({
     fetch: app.fetch,
     port,
--- apps/websocket/src/index.js
+++ apps/websocket/src/index.js
@@ -1,18 +1,13 @@
 import { serve } from "@hono/node-server";
 import { add } from "@repo/connect";
 import { Hono } from "hono";
-
 const app = new Hono();
-
 app.get("/", (c) => {
     return c.text("Hello Hono!");
 });
-
 const port = 3001;
 console.log(`Server is running on http://localhost:${port}`);
-
 console.log(add(1, 2));
-
 serve({
     fetch: app.fetch,
     port,
