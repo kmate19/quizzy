60219fb3870010fe6323e4d061e6e3432d2a3e87
kmate19 <kissmate019@gmail.com>
2024-11-28 10:48:35 +0100

    (feat): added tailwindcss to the frontend

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,12 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+//import tailwindcss from '@tailwindcss/vite'
+// ^ vite plugin currently breaks some styling with devtools and possibly other things so we'll use postcss for now
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    //tailwindcss(),
     vue(),
     vueDevTools(),
   ],
