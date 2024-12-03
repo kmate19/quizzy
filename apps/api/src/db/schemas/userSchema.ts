c24326be38bd993986f089453b677770fe9bf489
kmate19 <kissmate019@gmail.com>
2024-12-03 20:29:31 +0100

    feat+refactor: added user tokens and placed in separate files
    squash
    
    s
    
    s
    
    s

--- apps/api/src/db/schemas/userSchema.ts
+++
@@ -1,8 +0,0 @@
-import { serial, integer, pgTable, varchar } from "drizzle-orm/pg-core";
-
-export const usersTable = pgTable("users", {
-    id: serial().primaryKey(),
-    name: varchar({ length: 255 }).notNull(),
-    age: integer().notNull(),
-    email: varchar({ length: 255 }).notNull()
-});
