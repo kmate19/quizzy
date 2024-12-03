268fd5114ea782b0c3ce29328acbbd89c2a13d7b
kmate19 <kissmate019@gmail.com>
2024-12-03 20:49:54 +0100

    feat: added quizzes and cards table
    s

---
+++ apps/api/src/db/schemas/quizCardsSchema.ts
@@ -0,0 +1,20 @@
+import { relations, sql } from "drizzle-orm";
+import { pgTable, serial, smallint, timestamp, varchar } from "drizzle-orm/pg-core";
+import { quizzesTable } from "./quizzesSchema.ts";
+
+export const quizCardsTable = pgTable("quiz_cards", {
+    id: serial().primaryKey(),
+    quizId: serial().notNull().references(() => quizzesTable.id),
+    question: varchar({ length: 255 }).notNull(),
+    answer: varchar({ length: 255 }).notNull(),
+    correctAnswerIndex: smallint().notNull(),
+    createdAt: timestamp().notNull().defaultNow(),
+    updatedAt: timestamp().notNull().defaultNow().$onUpdate(() => sql`CURRENT_TIMESTAMP`),
+});
+
+export const quizCardsRelations = relations(quizCardsTable, ({ one }) => ({
+    quiz: one(quizzesTable, {
+        fields: [quizCardsTable.quizId],
+        references: [quizzesTable.id],
+    }),
+}));
