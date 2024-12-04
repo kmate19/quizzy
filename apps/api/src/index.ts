1a96066f699362c6c7e01513e9bf90f21018650b
kmate19 <kissmate019@gmail.com>
2024-12-04 12:36:29 +0100

    feat: created basic register route

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
+
 import db from "./db/index.ts";
 import { usersTable, type User } from "./db/schemas/usersSchema.ts";
 import { eq } from "drizzle-orm";
 
+import auth from "./routes/auth.ts";
 
-const app = new Hono();
+const app = new Hono().basePath("/api/v1");
 
 app.get("/", async (c) => {
     return c.text("Hello Hono!");
@@ -21,6 +23,8 @@ app.get('/test', async (c) => {
     await db.update(usersTable).set({ username: 'hono' }).where(eq(usersTable.username, 'Test'));
     return c.text('Test');
 });
+
+app.route('/', auth)
 
 const port = 3000;
 console.log(`Server is running on http://localhost:${port}`);
---
+++ apps/api/src/routes/auth.ts
@@ -0,0 +1,52 @@
+import db from "@/db/index.ts";
+import { LoginUserSchema, RegisterUserSchema, usersTable } from "@/db/schemas/usersSchema.ts";
+import postgresErrorHandler from "@/utils/postgresErrorHandler.ts";
+import { zValidator } from "@hono/zod-validator";
+import { genSalt, hash } from "bcrypt";
+import { Hono } from "hono";
+
+const auth = new Hono().basePath("/auth")
+
+    .post("/register", zValidator('json', RegisterUserSchema), async (c) => {
+        const userdata = c.req.valid('json')
+
+        // PERF: Probably would be better to check if the user is duplicate first and maybe use worker threads
+        try {
+            const saltrounds = 10;
+            const salt = await genSalt(saltrounds);
+            userdata.password = await hash(userdata.password, salt);
+        } catch (error) {
+            c.status(500);
+            return;
+        }
+
+        try {
+            await db.insert(usersTable).values(userdata);
+        } catch (error) {
+            const err = postgresErrorHandler(error as Error & { code: string });
+
+            if (err.type === "dup") {
+                c.status(400);
+                return c.json({ message: `${err.columnName} is already taken!` });
+            } else {
+                c.status(500);
+                return;
+            }
+        }
+
+        return c.text("Hello Auth!");
+    })
+
+    .post("/login", zValidator('json', LoginUserSchema), async (c) => {
+        return c.text("Hello Auth!");
+    })
+
+    .post("/logout", async (c) => {
+        return c.status(404)
+    })
+
+    .post("/refresh_token", async (c) => {
+        return c.status(404)
+    });
+
+export default auth;
