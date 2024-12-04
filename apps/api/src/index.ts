0c1f17004eb6f4befc923f65b0d12bce5339292a
kmate19 <kissmate019@gmail.com>
2024-12-04 14:13:36 +0100

    fix: removed dev stuff

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,25 +1,9 @@
 import { Hono } from "hono";
-import db from "./db/index.ts";
-import { usersTable, type User } from "./db/schemas/usersSchema.ts";
-import { eq } from "drizzle-orm";
-
 
 const app = new Hono();
 
 app.get("/", async (c) => {
     return c.text("Hello Hono!");
-});
-
-const testUser: User = {
-    username: "hono",
-    email: "hono@test.com",
-    password: "password",
-};
-
-app.get('/test', async (c) => {
-    await db.insert(usersTable).values(testUser);
-    await db.update(usersTable).set({ username: 'hono' }).where(eq(usersTable.username, 'Test'));
-    return c.text('Test');
 });
 
 const port = 3000;
