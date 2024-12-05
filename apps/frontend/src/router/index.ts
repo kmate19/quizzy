6a9a3b7a9415feb62fde87da8acd471137657499
Farkas <f.bence8100@gmail.com>
2024-12-05 19:12:05 +0100

    added background mist

--- apps/frontend/src/router/index.ts
+++ apps/frontend/src/router/index.ts
@@ -1,16 +1,16 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import TestView from '@/views/TestView.vue'
+import LoginRegisterView from '@/views/LoginRegister.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/test',
-      name: 'test',
+      path: '/',
+      name: 'loginRegister',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: TestView,
+      component: LoginRegisterView,
     },
   ],
 })
