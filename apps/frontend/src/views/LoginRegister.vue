af2b1cf80540fd67856b2398bede507c67759d7e
Farkas <f.bence8100@gmail.com>
2024-12-05 19:12:05 +0100

    added background mist

---
+++ apps/frontend/src/views/LoginRegister.vue
@@ -0,0 +1,121 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import MistBackground from '@/components/MistBackground.vue';
+
+const isLoginForm = ref(true);
+const isRegisterForm = ref(false);
+const password = ref('');
+const confirmPassword = ref('');
+
+const flipLogin = () => {
+  isLoginForm.value = !isLoginForm.value;
+  setTimeout(() => {
+    isRegisterForm.value = !isRegisterForm.value;
+  }, 350);
+};
+const flipRegister = () => {
+  isRegisterForm.value = !isRegisterForm.value;
+  setTimeout(() => {
+    isLoginForm.value = !isLoginForm.value;
+  }, 350);
+};
+
+const emailRules = [
+  (v: string) => !!v || 'Email kötelező',
+  (v: string) => /.+@.+\..+/.test(v) || 'Email formátum nem megfelelő'
+];
+
+const usernameRules = [
+  (v: string) => !!v || 'Felhasználónév kötelező',
+];
+
+const passwordRules = [
+  (v: string) => !!v || 'Jelszó kötelező',
+  (v: string) => v.length >= 8 || 'Jelszó minimum 8 karakter hosszú',
+  (v: string) => /[A-Z]/.test(v) || 'Jelszó tartalmazzon legalább egy nagybetűt',
+  (v: string) => /[a-z]/.test(v) || 'Jelszó tartalmazzon legalább egy kisbetűt',
+  (v: string) => /[!@#\$%\^\&*\)\(+=._-]/.test(v) || 'Jeleszó tartalmazzon legalább egy speciális karaktert',
+  (v: string) => /[0-9]/.test(v) || 'Jelszó tartalmazzon legalább egy számot'
+];
+
+const confirmPasswordRules = [
+(v: string) => !!v || 'Jelszó kötelező',
+(v: string) => /[A-Z]/.test(v) || 'Jelszó tartalmazzon legalább egy nagybetűt',
+(v: string) => /[a-z]/.test(v) || 'Jelszó tartalmazzon legalább egy kisbetűt',
+(v: string) => /[0-9]/.test(v) || 'Jelszó tartalmazzon legalább egy számot',
+(v: string) => /[!@#\$%\^\&*\)\(+=._-]/.test(v) || 'Jeleszó tartalmazzon legalább egy speciális karaktert',
+(v: string) => v.length >= 8 || 'Jelszó minimum 8 karakter hosszú'
+];
+</script>
+
+<template>
+  <MistBackground>
+    <div class="!flex !items-center !justify-center">
+      <v-card
+        class="!max-w-full !max-h-full !p-10 !rounded-2xl text-black "
+        prepend-icon="$login"
+        theme="dark"
+        :min-width="350"
+        :min-height="700"
+      >
+        <template v-slot:title>
+          <div class="min-w-full flex justify-evenly">
+          <span class="font-weight-black text-3xl">{{isLoginForm ? 'Bejelentkezés' : 'Regisztráció'}}</span>
+          </div>
+        </template>
+        <v-card-text class="bg-surface-light pt-4 rounded-xl p-24 h-fit">
+          <transition name="flip">
+            <div class="p-10" v-show="isLoginForm">
+              <v-text-field label="Felhasználónév" required></v-text-field>
+              <v-text-field label="Jelszó" type="password" required></v-text-field>
+              <div class="flex flex-wrap items-center">
+                <v-btn class="w-full !mt-5">Bejelentkezés</v-btn>
+                <br>
+                <v-btn class="w-full !mt-5" @click="flipLogin">Nincs fiókod? Regisztrálj!</v-btn>
+              </div>
+            </div>
+          </transition>
+          <transition name="flip">
+              <div class="p-10" v-show="isRegisterForm">
+              <v-text-field label="Email" :rules="emailRules" required></v-text-field>
+              <v-text-field label="Felhasználónév" :rules="usernameRules" required></v-text-field>
+              <v-text-field label="Jelszó" v-model="password" type="password" :rules="passwordRules" required></v-text-field>
+              <v-text-field label="Jelszó megerősítés" v-model="confirmPassword" type="password" :rules="confirmPasswordRules" required></v-text-field>
+              <div class="flex flex-wrap items-center">
+                <v-btn class="w-full !mt-5">Regisztráció</v-btn>
+                <br>
+                <v-btn class="w-full !mt-5" @click="flipRegister">Van már fiókod? Lépj be!</v-btn>
+              </div>
+              </div>
+          </transition>
+          <v-alert v-if="password !== confirmPassword" type="error" class="w-full">
+                A jelszavak nem egyeznek.
+                </v-alert>
+        </v-card-text>
+      </v-card>
+    </div>
+  </MistBackground>
+</template>
+
+<style>
+body{
+  background-color:floralwhite;
+}
+
+.flip-enter-active {
+  transform: scale(0);
+  transition: transform 0.4s;
+}
+.flip-enter-to {
+  transform: scale(1);
+}
+
+ .flip-leave-active {
+  transition: transform 0.4s;
+}
+
+.flip-leave-to {
+  transform: scale(0);
+}
+
+</style>
