fc1c0b54ec90b5ca8b487ab18436a44442bc577b
kmate19 <kissmate019@gmail.com>
2024-12-09 17:15:51 +0100

    feat: added email sender worker

--- apps/api/src/utils/email/sendEmail.ts
+++ apps/api/src/utils/email/sendEmail.ts
@@ -13,17 +13,31 @@ const transporter = createTransport({
 export default async function sendEmail(userEmail: string, emailToken: string) {
     console.log("Sending email to: ", userEmail);
     const mailOpts = {
-        from: `"Quizzy" <${ENV.EMAIL_USER}>`,
+        from: `"Quizzy" <${ENV.EMAIL_USER()}>`,
         to: userEmail,
         subject: "Test Email",
         html: createEmailTemplate(ENV.DOMAIN(), emailToken)
     }
 
-    try {
-        const info = await transporter.sendMail(mailOpts);
+    const sendMailWithTimeout = new Promise((resolve, reject) => {
+        const timeout = setTimeout(() => { reject(new Error("Email sending timed out")) }, 10000);
 
-        console.log("Message sent: ", info.response);
+        transporter.sendMail(mailOpts)
+            .then((info) => {
+                clearTimeout(timeout);
+                resolve(info);
+            })
+            .catch((error) => {
+                clearTimeout(timeout);
+                reject(error)
+            });
+    });
+
+    try {
+        const info = await sendMailWithTimeout;
+        console.log("Email sent: ", info);
     } catch (error) {
+        console.error("Failed to send email: ", error);
         throw error;
     }
 }
---
+++ apps/api/src/workers/emailWorker.ts
@@ -0,0 +1,27 @@
+declare var self: Worker;
+
+import sendEmail from "@/utils/email/sendEmail.ts";
+
+const RETRIES = 3;
+const RETRY_DELAY = 2000;
+self.onmessage = async (e) => {
+    const { email, emailToken } = e.data;
+
+    let attempts = 0;
+    while (attempts < RETRIES) {
+        try {
+            await sendEmail(email, emailToken);
+            break;
+        } catch (error) {
+            attempts++;
+            if (attempts >= RETRIES) {
+                console.error("Failed to send email after retries: ", error);
+                break;
+            }
+            console.log(`Retrying (${attempts}/${RETRIES})...`);
+
+            await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY));
+        }
+    }
+    self.terminate();
+}
