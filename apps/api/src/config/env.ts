855b371519ee99dbfaf6da3c46ae0957202875f7
kmate19 <kissmate019@gmail.com>
2024-12-11 08:46:09 +0100

    chore: useful messages

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -1,28 +1,31 @@
-function assertEnv<T>(env: T | undefined, name: string): T {
-    env = env === "" ? undefined : env;
-    if (env === undefined) {
-        throw new Error(name + " is not defined in the environment and has no default value!");
-    }
-    return env;
-}
-
 function assertEnvProd<T>(env: T | undefined, name: string): T | undefined {
     env = env === "" ? undefined : env;
     if (env === undefined && Bun.env.NODE_ENV === "production") {
         throw new Error(name + " is not defined in the environment and this is a production environment!");
     }
+    if (env === undefined) {
+        console.error(`WARN: ${name} is not defined in the environment, using default value!`);
+    }
     return env;
 }
 
+function verify() {
+    if (Bun.env.NODE_ENV !== "production") console.error("WARN: Running in development mode!");
+    for (const key in ENV) {
+        ENV[key as keyof typeof ENV]();
+    }
+}
+
 const ENV = {
-    NODE_ENV: () => assertEnv(Bun.env.NODE_ENV, "NODE_ENV"),
-    EMAIL_USER: () => assertEnv(Bun.env.EMAIL_USER, "EMAIL_USER"),
-    EMAIL_PASS: () => assertEnv(Bun.env.EMAIL_PASS, "EMAIL_PASS"),
-    DOMAIN: () => assertEnvProd(Bun.env.DOMAIN, "DOMAIN") || "http://localhost:3000",
+    NODE_ENV: () => assertEnvProd(Bun.env.NODE_ENV, "NODE_ENV") || "development",
+    EMAIL_USER: () => assertEnvProd(Bun.env.EMAIL_USER, "EMAIL_USER") || "",
+    EMAIL_PASS: () => assertEnvProd(Bun.env.EMAIL_PASS, "EMAIL_PASS") || "",
+    DOMAIN: () => assertEnvProd(Bun.env.DOMAIN, "DOMAIN") || "localhost",
     REFRESH_JWT_SECRET: () => assertEnvProd(Bun.env.REFRESH_JWT_SECRET, "REFRESH_JWT_SECRET") || "asd",
     ACCESS_JWT_SECRET: () => assertEnvProd(Bun.env.ACCESS_JWT_SECRET, "ACCESS_JWT_SECRET") || "asdf",
     DATABASE_URL: () => assertEnvProd(Bun.env.DATABASE_URL, "DATABASE_URL") || "postgres://postgres:mypassword@localhost:5432/postgres",
 } as const;
 
+verify();
 
 export default ENV;
