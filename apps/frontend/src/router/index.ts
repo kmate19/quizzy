424f87174f4e60eac4f52c043b4ee33fbb8bee69
Farkas <f.bence8100@gmail.com>
2024-12-13 15:32:14 +0100

    password visibility added, buttons, input fields design

--- apps/frontend/src/router/index.ts
+++ apps/frontend/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginRegisterView from '@/views/LoginRegister.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +14,11 @@ const router = createRouter({
       component: LoginRegisterView,
     },
     //redirect to login with alert on error
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView,
+    }
   ],
 })
 
---
+++ apps/frontend/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+    import MistBackground from '@/components/MistBackground.vue';
+    console.log("home view");
+</script>
+
+<template>
+    <MistBackground/>
+    <div class="home-view">
+        <h1>Welcome to HomeView</h1>
+        <p>This is the home page of the application.</p>
+    </div>
+</template>
+
+<style scoped>
+.home-view {
+    text-align: center;
+    margin-top: 50px;
+}
+</style>
