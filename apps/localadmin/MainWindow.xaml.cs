86f2821f527ca1273faceb9fed16c554792a5dd7
Gombási János Márk <gombasi.janos@ckik.loc>
2024-12-17 10:00:46 +0100

    created gui

--- apps/localadmin/MainWindow.xaml.cs
+++ apps/localadmin/MainWindow.xaml.cs
@@ -1,4 +1,9 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,13 +16,70 @@ using System.Windows.Shapes;
 
 namespace localadmin;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
-public partial class MainWindow : Window
-{
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
+        public ObservableCollection<User> Users { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Users = new ObservableCollection<User>
+            {
+                new User()
+                {
+                    Name = "Alice Smith",
+                    Email = "alice@example.com",
+                    Role = "Administrator"
+                },
+                new User()
+                {
+                    Name = "Bob Johnson",
+                    Email = "bob@example.com",
+                    Role = "Editor"
+                },
+                new User()
+                {
+                    Name = "Charlie Brown",
+                    Email = "charlie@example.com",
+                    Role = "Viewer"
+                },
+                new User()
+                {
+                    Name = "Diana Prince",
+                    Email = "diana@example.com",
+                    Role = "Editor"
+                },
+                new User()
+                {
+                    Name = "Edward Elric",
+                    Email = "edward@example.com",
+                    Role = "Viewer"
+                }
+            };
+
+
+            DataContext = this;
+        }
+
+        private void RedirectToMainPage(object sender, RoutedEventArgs e)
+        {   /*
+            string url = "https://www.google.com";
+            System.Diagnostics.Process.Start(new System.Diagnostics.ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            */
+
+            string url = "https://www.google.com";
+            WebBrowser wb = new WebBrowser();
+            wb.Navigate(new Uri(url));
+        }
     }
-}
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+
+        public User()
+        {
+        }
+    }
