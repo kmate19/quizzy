910bee0ad156224d403c8a522b7aea051e861d67
kmate19 <kissmate019@gmail.com>
2024-12-19 16:14:32 +0100

    chore: frontend lockfile + error see comment

--- apps/frontend/src/views/LoginRegister.vue
+++ apps/frontend/src/views/LoginRegister.vue
@@ -3,6 +3,8 @@ import { ref } from 'vue';
 import * as zod from 'zod';
 import router from '@/router';
 
+// FIX: ezt majd fixeld mert ennel a componentnel a setup nem ts, es ezert itt sir a ts es nem lehet buildelni,
+// tehat majd a componentek scriptjeit is tsbe kene irni maskepp error lesz,HomeViewnel ugyanez majd a setupot ott is ts-be ird at
 import MistBackground from '@/components/MistBackground.vue';
 import { baseRegisterSchema } from '@/schemas/RegistrationSchema';
 import { EyeIcon, EyeOffIcon } from 'lucide-vue-next';
@@ -30,28 +32,28 @@ type loginSchemaType = zod.infer<typeof loginvalidSchema>;
 type RegisterSchemaType = zod.infer<typeof baseRegisterSchema>;
 
 const createRegisterSchema = (password: string) =>
-baseRegisterSchema.refine(
-  (data) => data.confirmPassword === password,
-  { message: 'A jelszavak nem egyeznek', path: ['confirmPassword'] }
-);
+  baseRegisterSchema.refine(
+    (data) => data.confirmPassword === password,
+    { message: 'A jelszavak nem egyeznek', path: ['confirmPassword'] }
+  );
 const loginvalidSchema = zod.object({
   username: zod.string()
-    .min(1, { message: 'A mező kitöltése kötelező'}),
-  password: zod.string().min(1, { message: 'A mező kitöltése kötelező'})
+    .min(1, { message: 'A mező kitöltése kötelező' }),
+  password: zod.string().min(1, { message: 'A mező kitöltése kötelező' })
 });
 
-const regErrors = ref<zod.ZodFormattedError<RegisterSchemaType> | null >(null)
-const loginErrors = ref<zod.ZodFormattedError<loginSchemaType> | null >(null)
+const regErrors = ref<zod.ZodFormattedError<RegisterSchemaType> | null>(null)
+const loginErrors = ref<zod.ZodFormattedError<loginSchemaType> | null>(null)
 
 const onRegistration = () => {
   const schema = createRegisterSchema(regForm.value.password);
   const valid = schema.safeParse(regForm.value);
 
-  if(!valid.success) {
+  if (!valid.success) {
     regErrors.value = valid.error.format();
     console.log('Invalid form', regErrors.value);
   }
-  else{
+  else {
     regErrors.value = null;
     console.log('Valid form', regForm.value.email, regForm.value.username, regForm.value.password, regForm.value.confirmPassword);
     //toast show
@@ -61,11 +63,11 @@ const onRegistration = () => {
 const onLogin = () => {
   const valid = loginvalidSchema.safeParse(loginForm.value);
 
-  if(!valid.success) {
+  if (!valid.success) {
     loginErrors.value = valid.error.format();
     console.log('Invalid form', loginErrors.value);
   }
-  else{
+  else {
     loginErrors.value = null;
     console.log('Valid form', loginForm.value.username, loginForm.value.password);
     //localStorage.setItem('userData', JSON.stringify(api response));
@@ -82,152 +84,104 @@ const togglePassword = () => {
 </script>
 
 <template>
-  <MistBackground/>
-      <div class="wrapper">
-        <v-card
-          class="vcard !p-10 !rounded-2xl text-black  !bg-white/30
+  <MistBackground />
+  <div class="wrapper">
+    <v-card class="vcard !p-10 !rounded-2xl text-black  !bg-white/30
            bg-opacity-50 backdrop-blur-sm
-          !min-h-fit !min-w-fit !max-w-fit !max-h-fit"
-          :min-width="400"
-          theme="dark"
-        >
-          <template v-slot:title>
-            <div class="min-w-full flex justify-evenly">
-              <span class="font-weight-black text-3xl">{{isLoginForm ? 'Bejelentkezés' : 'Regisztráció'}}</span>
-            </div>
-          </template>
-        <div v-if="isLoginForm">
-          <form @submit.prevent="onLogin">
-              <v-text-field 
-              label="Felhasználónév" 
-              v-model="loginForm.username"
-              variant="outlined"
-              density="comfortable">
-            </v-text-field>
-                <span v-if="loginErrors" class="text-red-500">
-                    Helytelen felhasználónév vagy jelszó
-                </span>
-                  <v-text-field 
-                  name="pw" 
-                  label="Jelszó" 
-                  v-model="loginForm.password"
-                  variant="outlined"
-                  density="comfortable" 
-                  :type="showPassword ? 'text' : 'password'"
-                  :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"@click:append-inner="togglePassword"
-                  />
-                  <button
-                    @click="togglePassword"
-                    class="absolute right-2 top-1/2 transform 
+          !min-h-fit !min-w-fit !max-w-fit !max-h-fit" :min-width="400" theme="dark">
+      <template v-slot:title>
+        <div class="min-w-full flex justify-evenly">
+          <span class="font-weight-black text-3xl">{{ isLoginForm ? 'Bejelentkezés' : 'Regisztráció' }}</span>
+        </div>
+      </template>
+      <div v-if="isLoginForm">
+        <form @submit.prevent="onLogin">
+          <v-text-field label="Felhasználónév" v-model="loginForm.username" variant="outlined" density="comfortable">
+          </v-text-field>
+          <span v-if="loginErrors" class="text-red-500">
+            Helytelen felhasználónév vagy jelszó
+          </span>
+          <v-text-field name="pw" label="Jelszó" v-model="loginForm.password" variant="outlined" density="comfortable"
+            :type="showPassword ? 'text' : 'password'" :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+            @click:append-inner="togglePassword" />
+          <button @click="togglePassword" class="absolute right-2 top-1/2 transform
                     -translate-y-1/2 text-gray-500
-                    hover:text-gray-700 focus:outline-none"
-                    type="button"
-                  >
-                    <EyeIcon v-if="!showPassword" class="h-5 w-5" />
-                    <EyeOffIcon v-else class="h-5 w-5" />
-                  </button>
-                  <span v-if="loginErrors" class="text-red-500 !mt-0">
-                    Helytelen felhasználónév vagy jelszó
-                  </span>
-              <div class="flex flex-wrap items-center">
-                <v-btn type="submit" class="w-full !mt-5" variant="outlined">Bejelentkezés</v-btn>
-                <br>
-                <v-btn class="w-full !mt-5" @click="flipLogin" variant="outlined">Nincs fiókod? Regisztrálj!</v-btn>
-              </div>
-          </form>
-        </div>
-        <div v-else>
-          <form @submit.prevent="onRegistration">
-            <div class="">
-              <v-text-field
-              label="Email" 
-              name="email" 
-              v-model="regForm.email" 
-              required
-              variant="outlined"
+                    hover:text-gray-700 focus:outline-none" type="button">
+            <EyeIcon v-if="!showPassword" class="h-5 w-5" />
+            <EyeOffIcon v-else class="h-5 w-5" />
+          </button>
+          <span v-if="loginErrors" class="text-red-500 !mt-0">
+            Helytelen felhasználónév vagy jelszó
+          </span>
+          <div class="flex flex-wrap items-center">
+            <v-btn type="submit" class="w-full !mt-5" variant="outlined">Bejelentkezés</v-btn>
+            <br>
+            <v-btn class="w-full !mt-5" @click="flipLogin" variant="outlined">Nincs fiókod? Regisztrálj!</v-btn>
+          </div>
+        </form>
+      </div>
+      <div v-else>
+        <form @submit.prevent="onRegistration">
+          <div class="">
+            <v-text-field label="Email" name="email" v-model="regForm.email" required variant="outlined"
               density="comfortable"></v-text-field>
-              <span v-if="regErrors?.email" class="text-red-500">
-                {{regErrors.email._errors[0]}}
-              </span>
-              <v-text-field 
-              label="Felhasználónév" 
-              name="username" 
-              v-model="regForm.username"
-              required
-              variant="outlined"
+            <span v-if="regErrors?.email" class="text-red-500">
+              {{ regErrors.email._errors[0] }}
+            </span>
+            <v-text-field label="Felhasználónév" name="username" v-model="regForm.username" required variant="outlined"
               density="comfortable"></v-text-field>
-              <span v-if="regErrors?.username" class="text-red-500">
-                {{regErrors.username._errors[0]}}
-              </span>
-              <v-text-field 
-                label="Jelszó" 
-                v-model="regForm.password"
-                name="pw"
-                required
-                variant="outlined"
-                density="comfortable"
-                :type="showPassword ? 'text' : 'password'"
-                :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
-                @click:append-inner="togglePassword"
-              >
-              
-                <template v-slot:details>
-                  <div class="text-caption text-wrap mb-5">
-                    Min. 8 karakter, tartalmazzon kis- és nagybetűt, valamint speciális karaktert
-                  </div>
-                </template>
-              </v-text-field>
-              <button
-                @click="togglePassword"
-                class="absolute right-2 top-1/2 transform 
+            <span v-if="regErrors?.username" class="text-red-500">
+              {{ regErrors.username._errors[0] }}
+            </span>
+            <v-text-field label="Jelszó" v-model="regForm.password" name="pw" required variant="outlined"
+              density="comfortable" :type="showPassword ? 'text' : 'password'"
+              :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'" @click:append-inner="togglePassword">
+
+              <template v-slot:details>
+                <div class="text-caption text-wrap mb-5">
+                  Min. 8 karakter, tartalmazzon kis- és nagybetűt, valamint speciális karaktert
+                </div>
+              </template>
+            </v-text-field>
+            <button @click="togglePassword" class="absolute right-2 top-1/2 transform
                 -translate-y-1/2 text-gray-500
-                hover:text-gray-700 focus:outline-none"
-                type="button"
-              >
-                <EyeIcon v-if="!showPassword" class="h-5 w-5" />
-                <EyeOffIcon v-else class="h-5 w-5" />
-              </button>
-              <span v-if="regErrors?.password" class="text-red-500">
-                {{regErrors.password._errors[0]}}
-              </span>
-              <v-text-field 
-              label="Jelszó megerősítés" 
-              v-model="regForm.confirmPassword" 
-              :type="showPassword ? 'text' : 'password'"
-              :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"@click:append-inner="togglePassword"
-              name="pw"
-              required
-              variant="outlined"
+                hover:text-gray-700 focus:outline-none" type="button">
+              <EyeIcon v-if="!showPassword" class="h-5 w-5" />
+              <EyeOffIcon v-else class="h-5 w-5" />
+            </button>
+            <span v-if="regErrors?.password" class="text-red-500">
+              {{ regErrors.password._errors[0] }}
+            </span>
+            <v-text-field label="Jelszó megerősítés" v-model="regForm.confirmPassword"
+              :type="showPassword ? 'text' : 'password'" :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+              @click:append-inner="togglePassword" name="pw" required variant="outlined"
               density="comfortable"></v-text-field>
-              <span v-if="regErrors?.confirmPassword" class="text-red-500">
-                  {{regErrors.confirmPassword._errors[0]}}
-              </span>
-              <div class="flex flex-wrap items-center">
-                  <v-btn 
-                  class="w-full !mt-5"
-                  @click="onRegistration" variant="outlined">Regisztráció</v-btn>
-                <br>
-                <v-btn class="w-full !mt-5" @click="flipLogin" variant="outlined">Van már fiókod? Lépj be!</v-btn>
-              </div>
+            <span v-if="regErrors?.confirmPassword" class="text-red-500">
+              {{ regErrors.confirmPassword._errors[0] }}
+            </span>
+            <div class="flex flex-wrap items-center">
+              <v-btn class="w-full !mt-5" @click="onRegistration" variant="outlined">Regisztráció</v-btn>
+              <br>
+              <v-btn class="w-full !mt-5" @click="flipLogin" variant="outlined">Van már fiókod? Lépj be!</v-btn>
             </div>
-          </form>
-        </div>
-        </v-card>
-        <v-card class="headers !flex !flex-col !justify-center !items-center !text-center
+          </div>
+        </form>
+      </div>
+    </v-card>
+    <v-card class="headers !flex !flex-col !justify-center !items-center !text-center
           !bg-opacity-50
           !backdrop-blur-md
           !rounded-2xl
            !bg-white/30
           ">
-            <h1 class="title text-9xl text-white">Quizzy</h1>
-            <h3 class="quote text-5xl text-white">Fun way to learn haha</h3>
-        </v-card>
-      </div>
+      <h1 class="title text-9xl text-white">Quizzy</h1>
+      <h3 class="quote text-5xl text-white">Fun way to learn haha</h3>
+    </v-card>
+  </div>
 </template>
 
 <style scoped>
-
-.wrapper{
+.wrapper {
   height: 100vh;
   width: 100vw;
   display: flex;
@@ -236,7 +190,7 @@ const togglePassword = () => {
   padding: 5px;
 }
 
-.headers{
+.headers {
   height: 50%;
   width: 50%;
   align-self: center;
@@ -244,19 +198,21 @@ const togglePassword = () => {
   gap: 20px;
 }
 
-.vcard{
+.vcard {
   height: 50%;
   align-self: center;
 }
 
-.title{
+.title {
   font-size: 10vh;
   line-height: 1;
-} 
-.quote{
+}
+
+.quote {
   font-size: 6vh;
   line-height: 1;
 }
+
 v-text-field {
   width: 80vh;
 }
@@ -268,7 +224,8 @@ v-text-field {
     justify-content: flex-end;
     gap: 50px;
   }
-  .headers{
+
+  .headers {
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
@@ -276,10 +233,12 @@ v-text-field {
     padding: 10px;
     height: 25%;
   }
-  .headers h1{
+
+  .headers h1 {
     font-size: 8vh;
   }
-  .headers h3{
+
+  .headers h3 {
     font-size: 3vh;
   }
 }
