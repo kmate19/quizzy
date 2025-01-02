d963ba42fc91ad4766a13133c66d7a41280d932c
kmate19 <kissmate019@gmail.com>
2025-01-02 17:28:43 +0100

    fix: env management

--- apps/api/src/config/env.ts
+++ apps/api/src/config/env.ts
@@ -1,10 +1,12 @@
-function assertEnvProd<T>(env: T | undefined, name: string): T | undefined {
+function assertEnvProd(env: string | undefined, name: string, defval: string): string | void {
     env = env === "" ? undefined : env;
     if (env === undefined && Bun.env.NODE_ENV === "production") {
         throw new Error(name + " is not defined in the environment and this is a production environment!");
     }
     if (env === undefined) {
-        console.error(`WARN: ${name} is not defined in the environment, using default value!`);
+        if (defval === "") throw new Error(name + " is not defined in the environment and has no default value!");
+        console.error(`WARN: ${name} is not defined in the environment, using default value: "${defval}" !`);
+        env = defval;
     }
     return env;
 }
@@ -17,13 +19,13 @@ function verify() {
 }
 
 const ENV = {
-    NODE_ENV: () => assertEnvProd(Bun.env.NODE_ENV, "NODE_ENV") || "development",
-    EMAIL_USER: () => assertEnvProd(Bun.env.EMAIL_USER, "EMAIL_USER") || "",
-    EMAIL_PASS: () => assertEnvProd(Bun.env.EMAIL_PASS, "EMAIL_PASS") || "",
-    DOMAIN: () => assertEnvProd(Bun.env.DOMAIN, "DOMAIN") || "localhost",
-    REFRESH_JWT_SECRET: () => assertEnvProd(Bun.env.REFRESH_JWT_SECRET, "REFRESH_JWT_SECRET") || "asd",
-    ACCESS_JWT_SECRET: () => assertEnvProd(Bun.env.ACCESS_JWT_SECRET, "ACCESS_JWT_SECRET") || "asdf",
-    DATABASE_URL: () => assertEnvProd(Bun.env.DATABASE_URL, "DATABASE_URL") || "postgres://postgres:mypassword@localhost:5432/postgres",
+    NODE_ENV: () => assertEnvProd(Bun.env.NODE_ENV, "NODE_ENV", "development"),
+    EMAIL_USER: () => assertEnvProd(Bun.env.EMAIL_USER, "EMAIL_USER", "asd"),
+    EMAIL_PASS: () => assertEnvProd(Bun.env.EMAIL_PASS, "EMAIL_PASS", "asd"),
+    DOMAIN: () => assertEnvProd(Bun.env.DOMAIN, "DOMAIN", "localhost"),
+    REFRESH_JWT_SECRET: () => assertEnvProd(Bun.env.REFRESH_JWT_SECRET, "REFRESH_JWT_SECRET", "asd"),
+    ACCESS_JWT_SECRET: () => assertEnvProd(Bun.env.ACCESS_JWT_SECRET, "ACCESS_JWT_SECRET", "asdf"),
+    DATABASE_URL: () => assertEnvProd(Bun.env.DATABASE_URL, "DATABASE_URL", "postgres://postgres:mypassword@localhost:5432/postgres"),
 } as const;
 
 verify();
