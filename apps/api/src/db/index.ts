da38dc0fafb9c52f13b3cb25d3f94d2dd7d45683
kmate19 <kissmate019@gmail.com>
2025-01-07 19:36:30 +0100

    feat: added mock data generation using drizzle seed !

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -3,6 +3,7 @@ import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import * as schema from './schemas/index.ts';
 import ENV from '@/config/env.ts';
 import GLOBALS from '@/config/globals.ts';
+import { reset, seed } from 'drizzle-seed';
 
 const opts = ENV.NODE_ENV() === 'production' ? { schema } : { logger: true, schema };
 
@@ -13,10 +14,15 @@ if (ENV.NODE_ENV() === 'production') {
     await migrate(db, { migrationsFolder: './migrations' });
 } else {
     await db.execute('SELECT 1');
+    await reset(db, schema);
+    // currently quizzes dont work since bytea is not supported
+    const { quizzesTable, reviewsTable, quizLanguagesTable, quizTagsTable, quizCardsTable, ...rest } = schema;
+    await seed(db, rest, { count: 100 });
 }
 
 for (let i = 0; i < GLOBALS.DB_ROLES.length; i++) {
     await db.insert(schema.rolesTable).values(GLOBALS.DB_ROLES[i]).onConflictDoNothing()
 }
+
 
 export default db;
