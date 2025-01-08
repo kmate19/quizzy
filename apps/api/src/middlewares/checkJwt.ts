5d5ab094ce97e27ed8a7df9f001f71e10e960097
kmate19 <kissmate019@gmail.com>
2025-01-08 17:31:19 +0100

    feat: improved middleware
    s

--- apps/api/src/middlewares/checkJwt.ts
+++ apps/api/src/middlewares/checkJwt.ts
@@ -5,6 +5,7 @@ import { usersTable } from "@/db/schemas/usersSchema.ts";
 import { userTokensTable } from "@/db/schemas/userTokensSchema.ts";
 import type { QuizzyJWTPAYLOAD } from "@/types.ts";
 import { eq } from "drizzle-orm";
+import type { Context } from "hono";
 import { getCookie, setCookie } from "hono/cookie";
 import { createMiddleware } from "hono/factory";
 import { decode, sign, verify } from "hono/jwt";
@@ -13,58 +14,59 @@ import { JwtTokenExpired } from "hono/utils/jwt/types";
 
 const authJwtMiddleware = (role?: string) => {
     return createMiddleware<{ Variables: { accessTokenPayload: QuizzyJWTPAYLOAD } }>(async (c, next) => {
-        let accessCookie: string | undefined;
-        let accessTokenPayload: QuizzyJWTPAYLOAD | null = null;
-        try {
-            accessCookie = getCookie(c, GLOBALS.ACCESS_COOKIE_NAME)
+        const accessCookie = getCookie(c, GLOBALS.ACCESS_COOKIE_NAME)
 
-            if (!accessCookie) {
-                return c.redirect("/login");
-            }
-
-            accessTokenPayload = await verify(accessCookie, ENV.ACCESS_JWT_SECRET()) as QuizzyJWTPAYLOAD;
-
-            if (role) {
-                const userRole = await db.query.usersTable.findFirst({
-                    where: eq(usersTable.id, accessTokenPayload.userId),
-                    with: {
-                        roles: {
-                            with: {
-                                role: true
-                            }
-                        }
-                    }
-                });
-                if (!userRole?.roles.some(r => r.role.name === role)) {
-                    c.status(404);
-                    return c.redirect("/404");
-                }
-            }
-        } catch (error) {
-            console.log(error)
-            if (error instanceof JwtTokenExpired && accessCookie) {
-                const { payload } = decode(accessCookie) as { payload: QuizzyJWTPAYLOAD, header: TokenHeader };
-
-                const res = await db.select().from(userTokensTable).where(eq(userTokensTable.id, payload.refreshTokenId))
-                if (!res.length) {
-                    return c.redirect("/login");
-                }
-
-                accessTokenPayload = { userId: payload.userId, refreshTokenId: payload.refreshTokenId, exp: Math.floor(Date.now() / 1000) + 60 * 30 };
-
-                const accessToken = await sign(accessTokenPayload, ENV.ACCESS_JWT_SECRET())
-
-                setCookie(c, GLOBALS.ACCESS_COOKIE_NAME, accessToken, GLOBALS.COOKIE_OPTS);
-            }
-        }
-
-        if (!accessTokenPayload) {
-            c.status(500)
+        if (!accessCookie) {
             return c.redirect("/login");
         }
-        c.set("accessTokenPayload", accessTokenPayload);
+
+        const accessTokenPayloadOrError = await verify(accessCookie, ENV.ACCESS_JWT_SECRET()).catch(e => e) as QuizzyJWTPAYLOAD;
+
+        if (accessTokenPayloadOrError instanceof JwtTokenExpired) {
+            refreshAccessToken(c, accessCookie)
+        } else if (accessTokenPayloadOrError instanceof Error) {
+            console.log(accessTokenPayloadOrError);
+            c.status(500);
+            return c.redirect("/login");
+        }
+
+        if (role) {
+            const [userAndRoles] = await db.query.usersTable.findMany({
+                where: eq(usersTable.id, accessTokenPayloadOrError.userId),
+                with: {
+                    roles: {
+                        with: {
+                            role: true
+                        }
+                    }
+                }
+            });
+
+            if (!userAndRoles.roles.some(r => r.role.name === role)) {
+                c.status(404);
+                return c.redirect("/404");
+            }
+        }
+
+        c.set("accessTokenPayload", accessTokenPayloadOrError);
         await next();
     })
 };
+
+async function refreshAccessToken(c: Context, accessCookie: string) {
+    const { payload } = decode(accessCookie) as { payload: QuizzyJWTPAYLOAD, header: TokenHeader };
+
+    // TODO: Check if the refresh token is still valid
+    const res = await db.select().from(userTokensTable).where(eq(userTokensTable.id, payload.refreshTokenId))
+    if (!res.length) {
+        return c.redirect("/login");
+    }
+
+    const newAccessTokenPayload = { userId: payload.userId, refreshTokenId: payload.refreshTokenId, exp: Math.floor(Date.now() / 1000) + 60 * 30 };
+
+    const accessToken = await sign(newAccessTokenPayload, ENV.ACCESS_JWT_SECRET())
+
+    setCookie(c, GLOBALS.ACCESS_COOKIE_NAME, accessToken, GLOBALS.COOKIE_OPTS);
+}
 
 export default authJwtMiddleware;
