7b0666ed0727512eab6951cbef228a1f415c0f66
Farkas <f.bence8100@gmail.com>
2025-01-13 20:04:08 +0100

    added toast, now it can be accesed from anywhere, fixed password show btn, text area adjustments

--- apps/frontend/src/main.ts
+++ apps/frontend/src/main.ts
@@ -3,8 +3,12 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { createVuetify } from 'vuetify'
+
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+
+import Vue3Toastify, {type ToastContainerOptions} from 'vue3-toastify'
+import 'vue3-toastify/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
@@ -19,5 +23,9 @@ const vuetify = createVuetify({
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
+app.use(Vue3Toastify, {
+    autoClose: 5000,
+  } as ToastContainerOptions
+)
 
 app.mount('#app')
