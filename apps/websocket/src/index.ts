46ea0bbae55fb72352576935592b01f41652ea96
kmate19 <kissmate019@gmail.com>
2025-01-21 13:41:26 +0100

    chore: fix docker image

--- apps/websocket/src/index.ts
+++ apps/websocket/src/index.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-import { add } from "@repo/connect";
 
 
 const app = new Hono();
@@ -11,8 +10,6 @@ app.get("/", (c) => {
 const port = 3001;
 console.log(Bun.env.PATH);
 console.log(`Server is running on http://localhost:${port}`);
-
-console.log(add(6, 2));
 
 export default {
     port: port,
