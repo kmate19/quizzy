fd78a250c70a4eafb2129c67e46388600f8b7247
kmate19 <kissmate019@gmail.com>
2025-01-24 20:47:48 +0100

    tests+feat: hash checking and message type parsing with zod

---
+++ apps/websocket/src/schemas/zodschemas.ts
@@ -0,0 +1,30 @@
+import { Equal } from "hono/utils/types";
+import type { WebsocketMessage, WebsocketMessageType } from "repo";
+import { z } from "zod";
+
+const wsMessageTypeZEnum = z.enum(["message", "subscribe", "unsubscribe", "ping", "pong", "ack", "connect", "disconnect", "handshake", "error"]);
+
+// ignore this, just doing typescript magic to check if the types are correct
+function assertequal<T extends true>() { }
+let _valid1: Equal<z.infer<typeof wsMessageTypeZEnum>, WebsocketMessageType>;
+let _valid2: Equal<z.infer<typeof websocketMessageSchema>, WebsocketMessage>;
+assertequal<typeof _valid1>();
+assertequal<typeof _valid2>();
+
+export const websocketMessageSchema = z.object({
+    type: wsMessageTypeZEnum,
+    successful: z.boolean(),
+    errMessage: z.string().optional(),
+    data: z.unknown().optional(),
+    clientId: z.string().optional(),
+    ext: z.object({
+        ack: z.boolean().optional(),
+        timesync: z.object({
+            l: z.number(),
+            o: z.number(),
+            tc: z.number().optional(),
+            ts: z.number().optional(),
+            p: z.number().optional(),
+        }).optional(),
+    }).optional(),
+});
