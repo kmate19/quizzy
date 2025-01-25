9937312589d07f07c7322723fcf20b9464e7c75c
kmate19 <kissmate019@gmail.com>
2025-01-25 13:03:38 +0100

    feat: added hono ws client

---
+++ apps/websocket/src/hc.ts
@@ -0,0 +1,10 @@
+import { app } from '@/index'
+
+import { hc } from 'hono/client'
+
+// this is a trick to calculate the type when compiling
+const client = hc<typeof app>('')
+export type Client = typeof client
+
+export const wsHcWithType = (...args: Parameters<typeof hc>): Client =>
+    hc<typeof app>(...args)
