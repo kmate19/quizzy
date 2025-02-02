b0597f353bd7c7e761c8beba135070da32176656
Farkas <f.bence8100@gmail.com>
2025-02-02 16:02:05 +0100

    added bg inside glass like container as KGergo02 asked

--- apps/frontend/src/views/ProfileView.vue
+++ apps/frontend/src/views/ProfileView.vue
@@ -7,10 +7,6 @@ import { clientv1 } from '@/lib/apiClient'
 import router from '@/router'
 import { toast, type ToastOptions } from 'vue3-toastify'
 
-const asd = () => {
-  alert('asd')
-}
-
 const user = {
   playedgames: 200,
   games_won: 56,
@@ -131,217 +127,236 @@ const closePasswordModal = () => {
 
 const handlePasswordChange = async () => {
   if (passwordForm.value.new !== passwordForm.value.confirm) {
-    toast("A jelszavak nem egyeznek", {
+    toast('A jelszavak nem egyeznek', {
       autoClose: 5000,
       position: toast.POSITION.TOP_CENTER,
       type: 'error',
       transition: 'zoom',
       pauseOnHover: false,
     } as ToastOptions)
-    
+
     return
   }
 
   await new Promise((resolve) => setTimeout(resolve, 1000))
   closePasswordModal()
 }
-
 </script>
 
 <template>
-    <div class="fixed inset-0 pointer-events-none overflow-hidden">
-      <MistBackground />
+  <div class="fixed inset-0 pointer-events-none overflow-hidden">
+    <MistBackground />
+  </div>
+  <div class="relative max-w-7xl mx-auto">
+    <NavBar />
+    <div class="backdrop-blur-md bg-white/10 rounded-2xl p-8 mb-8 flex flex-wrap gap-8">
+      <div class="flex flex-wrap items-center gap-8">
+        <div class="relative">
+          <img
+            :src="profileImage || ''"
+            class="w-32 h-32 rounded-full object-cover border-4 border-white/30"
+          />
+          <div
+            class="absolute -top-2 -right-2 p-2 rounded-full bg-white/10 backdrop-blur-sm cursor-pointer hover:bg-white/20 transition-colors"
+            @click="openFileDialog"
+          >
+            <PencilIcon class="w-5 h-5 text-white" />
+          </div>
+          <input
+            type="file"
+            ref="fileInput"
+            class="hidden"
+            accept="image/*"
+            @change="handleFileChange"
+          />
+          <button
+            v-if="showSaveButton"
+            @click="saveProfileImage"
+            class="absolute -bottom-2 -right-2 px-3 py-1 bg-green-500 text-white text-sm rounded-full hover:bg-green-600 transition-colors"
+          >
+            Mentés
+          </button>
+        </div>
+        <div class="text-white flex flex-col flex-wrap">
+          <h1 class="text-3xl font-bold mb-2">{{ user.username }}</h1>
+          <p class="text-white/80">{{ user.email }}</p>
+          <p
+            v-if="user.role === 'admin'"
+            class="mt-2 px-3 py-1 bg-purple-500/30 rounded-full inline-block text-sm"
+          >
+            {{ user.role }}
+          </p>
+        </div>
+      </div>
+      <div class="flex-1 flex flex-wrap justify-end items-center gap-4">
+        <div class="text-center">
+          <div class="text-4xl font-bold text-white mb-2">{{ user.playedgames }}</div>
+          <div class="text-white/70 text-sm">Játszmák</div>
+        </div>
+        <div class="text-center">
+          <div class="text-4xl font-bold text-white mb-2">{{ user.games_won }}</div>
+          <div class="text-white/70 text-sm">1. helyezés</div>
+        </div>
+        <div class="text-center">
+          <div class="text-4xl font-bold text-white mb-2">
+            {{ Math.round((user.games_won / user.playedgames) * 100) }}%
+          </div>
+          <div class="text-white/70 text-sm">Nyerési arány</div>
+        </div>
+        <div class="flex gap-4">
+          <button
+            @click="openPasswordModal"
+            class="glass-button px-4 py-1 text-lg text-white font-semibold rounded-lg transition-all duration-300 ease-in-out cursor-pointer w-fit !bg-green-900"
+          >
+            Jelszó módosítás
+          </button>
+          <button
+            class="glass-button px-4 py-1 text-lg text-white font-semibold rounded-lg transition-all duration-300 ease-in-out cursor-pointer w-fit !bg-red-900"
+            @click="OnLogOut"
+          >
+            Kijelentkezés
+          </button>
+        </div>
+      </div>
     </div>
-    <div class="relative max-w-7xl mx-auto">
-      <NavBar />
-      <div class="backdrop-blur-md bg-white/10 rounded-2xl p-8 mb-8 flex flex-wrap gap-8 ">
-        <div class="flex flex-wrap items-center gap-8">
-          <div class="relative">
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
+      <div class="backdrop-blur-md bg-white/10 rounded-2xl p-6">
+        <h2 class="text-2xl font-bold text-white mb-6 flex items-center justify-between">
+          Barátok
+          <span class="text-sm font-normal text-white/70">
+            {{ user.friends.length }} összesen
+          </span>
+        </h2>
+        <div class="space-y-4 overflow-y-scroll custom-scrollbar p-6" style="max-height: 400px">
+          <div
+            v-for="friend in user.friends"
+            :key="friend.name"
+            class="flex gap-4 p-2 rounded-xl hover:bg-white/20 h-32
+             text-white ease-in-out bg-multi-color-gradient
+              border-white border-4 shadow-lg hover:-translate-y-0.5 transition-all duration-300"
+          >
             <img
-              :src="profileImage || ''"
-              class="w-32 h-32 rounded-full object-cover border-4 border-white/30"
+              :src="friend.pfp"
+              alt="Friend profile"
+              class="w-20 h-20 rounded-full object-cover"
             />
-            <div
-              class="absolute -top-2 -right-2 p-2 rounded-full bg-white/10 backdrop-blur-sm cursor-pointer hover:bg-white/20 transition-colors"
-              @click="openFileDialog"
-            >
-              <PencilIcon class=" w-5 h-5 text-white" />
+            <div class="flex-1">
+              <h3 class="text-white font-medium text-xl mb-2">{{ friend.name }}</h3>
+              <p class="text-sm">
+                <span
+                  class="inline-block w-2 h-2 rounded-full mr-2"
+                  :class="friend.activity_status === 'online' ? 'bg-green-400' : 'bg-gray-400'"
+                >
+                </span>
+                {{ friend.activity_status }}
+              </p>
             </div>
-            <input
-              type="file"
-              ref="fileInput"
-              class="hidden"
-              accept="image/*"
-              @change="handleFileChange"
-            />
-            <button
-              v-if="showSaveButton"
-              @click="saveProfileImage"
-              class="absolute -bottom-2 -right-2 px-3 py-1 bg-green-500 text-white text-sm rounded-full hover:bg-green-600 transition-colors"
-            >
-              Mentés
-            </button>
-          </div>
-          <div class="text-white flex flex-col flex-wrap">
-            <h1 class="text-3xl font-bold mb-2">{{ user.username }}</h1>
-            <p class="text-white/80">{{ user.email }}</p>
-            <p
-              v-if="user.role === 'admin'"
-              class="mt-2 px-3 py-1 bg-purple-500/30 rounded-full inline-block text-sm"
-            >
-              {{ user.role }}
-            </p>
-          </div>
-        </div>
-        <div class="flex-1 flex flex-wrap justify-end items-center gap-4">
-          <div class="text-center">
-            <div class="text-4xl font-bold text-white mb-2">{{ user.playedgames }}</div>
-            <div class="text-white/70 text-sm">Játszmák</div>
-          </div>
-          <div class="text-center">
-            <div class="text-4xl font-bold text-white mb-2">{{ user.games_won }}</div>
-            <div class="text-white/70 text-sm">1. helyezés</div>
-          </div>
-          <div class="text-center">
-            <div class="text-4xl font-bold text-white mb-2">
-              {{ Math.round((user.games_won / user.playedgames) * 100) }}%
-            </div>
-            <div class="text-white/70 text-sm">Nyerési arány</div>
-          </div>
-          <div class="flex gap-4">
-            <button
-              @click="openPasswordModal"
-              class="glass-button px-4 py-1 text-lg text-white font-semibold rounded-lg transition-all duration-300 ease-in-out cursor-pointer w-fit !bg-green-900"
-            >
-              Jelszó módosítás
-            </button>
-            <button
-              class="glass-button px-4 py-1 text-lg text-white font-semibold rounded-lg transition-all duration-300 ease-in-out cursor-pointer w-fit !bg-red-900"
-              @click="OnLogOut"
-            >
-              Kijelentkezés
-            </button>
           </div>
         </div>
       </div>
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
-        <div class="backdrop-blur-md bg-white/10 rounded-2xl p-6 ">
-          <h2 class="text-2xl font-bold text-white mb-6 flex items-center justify-between">
-            Barátok
-            <span class="text-sm font-normal text-white/70">
-              {{ user.friends.length }} összesen
-            </span>
-          </h2>
-          <div class="space-y-4 overflow-y-scroll custom-scrollbar p-6" style="max-height: 400px">
-            <div
-              v-for="friend in user.friends"
-              :key="friend.name"
-              class="flex gap-4 p-2 rounded-xl hover:bg-white/20 h-32 text-white ease-in-out
-      bg-white/10 border-white/30 border-4 shadow-lg hover:-translate-y-0.5 transition-all duration-300"
-            >
-              <img
-                :src="friend.pfp"
-                alt="Friend profile"
-                class="w-20 h-20 rounded-full object-cover"
-              />
-              <div class="flex-1">
-                <h3 class="text-white font-medium text-xl mb-2">{{ friend.name }}</h3>
-                <p class="text-sm">
-                  <span
-                    class="inline-block w-2 h-2 rounded-full mr-2"
-                    :class="friend.activity_status === 'online' ? 'bg-green-400' : 'bg-gray-400'"
-                  >
-                  </span>
-                  {{ friend.activity_status }}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="backdrop-blur-md bg-white/10 rounded-2xl p-6">
-          <h2 class="text-2xl font-bold text-white mb-6 flex items-center justify-between">
-            Saját quizzyk
-            <span class="text-sm font-normal text-white/70">
-              {{ user.quizzes.length }} összesen
-            </span>
-          </h2>
-          <div class="space-y-4 overflow-y-auto custom-scrollbar p-6" style="max-height: 400px">
-            <div
-              @click="asd()"
-              v-for="quiz in user.quizzes"
-              :key="quiz.name"
-              class="quizzy flex gap-4 p-2 rounded-xl hover:bg-white/20 h-32 text-white ease-in-out
-              bg-white/10 border-white/30 border-4 shadow-lg hover:-translate-y-0.5 transition-all duration-300"
-            >
-              <img
-                :src="quiz.quize_img"
-                alt="Quiz thumbnail"
-                class="w-20 h-20 rounded-lg object-cover"
-              />
-              <div class="flex-1">
-                <h3 class="text-white font-medium text-base">{{ quiz.name }}</h3>
-                <p class="text-sm text-white/70 mb-2">Kategória: {{ quiz.category }}</p>
-                <p class="text-sm">
-                  <span class="text-purple-300">{{ quiz.games }}</span> alkalommal játszott
-                </p>
-              </div>
+      <div class="backdrop-blur-md bg-white/10 rounded-2xl p-6">
+        <h2 class="text-2xl font-bold text-white mb-6 flex items-center justify-between">
+          Saját quizzyk
+          <span class="text-sm font-normal text-white/70">
+            {{ user.quizzes.length }} összesen
+          </span>
+        </h2>
+        <div class="space-y-4 overflow-y-auto custom-scrollbar p-6" style="max-height: 400px">
+          <div
+            v-for="quiz in user.quizzes"
+            :key="quiz.name"
+            class="quizzy flex gap-4 p-2 rounded-xl h-32 text-white ease-in-out border-white border-4 shadow-lg hover:-translate-y-0.5 transition-all duration-300
+            bg-multi-color-gradient"
+          >
+            <img
+              :src="quiz.quize_img"
+              alt="Quiz thumbnail"
+              class="w-20 h-20 rounded-lg object-cover"
+            />
+            <div class="flex-1">
+              <h3 class="text-white font-medium text-base">{{ quiz.name }}</h3>
+              <p class="text-sm text-white/70 mb-2">Kategória: {{ quiz.category }}</p>
+              <p class="text-sm">
+                <span class="text-purple-300">{{ quiz.games }}</span> alkalommal játszott
+              </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-    >
-    <div
-      v-if="showPasswordModal"
-      class="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center 
-      justify-center z-50"
-    >
-      <div class="bg-white/10 backdrop-blur-md p-8 rounded-2xl w-full max-w-md">
-        <div class="flex justify-between items-center mb-6">
-          <h3 class="text-2xl font-bold text-white">Jelszó változtatás</h3>
-          <XIcon
-            @click="closePasswordModal"
-            class="w-6 h-6 text-white cursor-pointer hover:text-white/70"
+  </div>
+  >
+  <div
+    v-if="showPasswordModal"
+    class="fixed inset-0 bg-black/50 backdrop-blur-sm flex items-center justify-center z-50"
+  >
+    <div class="bg-white/10 backdrop-blur-md p-8 rounded-2xl w-full max-w-md">
+      <div class="flex justify-between items-center mb-6">
+        <h3 class="text-2xl font-bold text-white">Jelszó változtatás</h3>
+        <XIcon
+          @click="closePasswordModal"
+          class="w-6 h-6 text-white cursor-pointer hover:text-white/70"
+        />
+      </div>
+      <form @submit.prevent="handlePasswordChange" class="space-y-4">
+        <div>
+          <label class="block text-white mb-2">Jelenlegi jelszó</label>
+          <input
+            type="text"
+            v-model="passwordForm.current"
+            class="w-full px-4 py-2 rounded-lg bg-white/5 border border-white/10 text-white focus:outline-none focus:border-white/30"
           />
         </div>
-        <form @submit.prevent="handlePasswordChange" class="space-y-4">
-          <div>
-            <label class="block text-white mb-2">Jelenlegi jelszó</label>
-            <input
-              type="text"
-              v-model="passwordForm.current"
-              class="w-full px-4 py-2 rounded-lg bg-white/5 border
-               border-white/10 text-white focus:outline-none focus:border-white/30"
-            />
-          </div>
-          <div>
-            <label class="block text-white mb-2">Új jelszó</label>
-            <input
-              type="text"
-              v-model="passwordForm.new"
-              class="w-full px-4 py-2 rounded-lg bg-white/5 border border-white/10 text-white focus:outline-none focus:border-white/30"
-            />
-          </div>
-          <div>
-            <label class="block text-white mb-2">Új jelszó megerősítése</label>
-            <input
-              type="text"
-              v-model="passwordForm.confirm"
-              class="w-full px-4 py-2 rounded-lg bg-white/5 border border-white/10 text-white focus:outline-none focus:border-white/30"
-            />
-          </div>
-          <button
-            type="submit"
-            class="glass-button px-4 py-1 text-lg text-white font-semibold rounded-lg transition-all duration-300 ease-in-out cursor-pointer w-full !bg-green-900"
-          >
-            Jelszó módosítása
-          </button>
-        </form>
-      </div>
+        <div>
+          <label class="block text-white mb-2">Új jelszó</label>
+          <input
+            type="text"
+            v-model="passwordForm.new"
+            class="w-full px-4 py-2 rounded-lg bg-white/5 border border-white/10 text-white focus:outline-none focus:border-white/30"
+          />
+        </div>
+        <div>
+          <label class="block text-white mb-2">Új jelszó megerősítése</label>
+          <input
+            type="text"
+            v-model="passwordForm.confirm"
+            class="w-full px-4 py-2 rounded-lg bg-white/5 border border-white/10 text-white focus:outline-none focus:border-white/30"
+          />
+        </div>
+        <button
+          type="submit"
+          class="glass-button px-4 py-1 text-lg text-white font-semibold rounded-lg transition-all duration-300 ease-in-out cursor-pointer w-full !bg-green-900"
+        >
+          Jelszó módosítása
+        </button>
+      </form>
     </div>
+  </div>
 </template>
 
 <style>
+.bg-multi-color-gradient {
+  background: linear-gradient(
+    90deg,
+    rgba(255, 0, 0, 0.5),
+    rgba(0, 255, 0, 0.5),
+    rgba(0, 0, 255, 0.5),
+    rgba(238, 238, 85, 0.5),
+    rgba(255, 0, 0, 0.5)
+  );
+  background-size: 400% 100%;
+  animation: gradient 20s linear infinite;
+}
+
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  100% {
+    background-position: 400% 50%;
+  }
+}
+
 .custom-scrollbar {
   scrollbar-width: thin;
   scrollbar-color: rgba(255, 255, 255, 0.3) rgba(255, 255, 255, 0.1);
