7793991a07ebf3e83bbbcbdff73222d131037f89
kmate19 <kissmate019@gmail.com>
2025-02-03 19:33:51 +0100

    feat: proxy fixx

--- apps/frontend/src/lib/apiClient.ts
+++ apps/frontend/src/lib/apiClient.ts
@@ -2,7 +2,7 @@ import { hcWithType } from "@repo/api/hc"
 import { wsHcWithType } from "@repo/websocket/hc";
 
 // TODO: change domain in prod
-export const clientv1 = hcWithType("/").api.v1;
+export const clientv1 = hcWithType("http://localhost:5173/").api.v1;
 
-export const wsclient = wsHcWithType("/").ws;
+export const wsclient = wsHcWithType("http://localhost:5173/").ws;
 
--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -2,15 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { visualizer } from 'rollup-plugin-visualizer'
 
 import { defineConfig } from 'vite'
-//import tailwindcss from '@tailwindcss/vite'
-// ^ vite plugin currently breaks some styling with devtools and possibly other things so we'll use postcss for now
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    //tailwindcss(),
+    tailwindcss(),
     vue(),
     vueDevTools(),
     visualizer({ open: true })
@@ -24,6 +23,7 @@ export default defineConfig({
       "/ws": {
         target: "http://localhost:3001",
         changeOrigin: true,
+        ws: true,
       }
     },
   },
