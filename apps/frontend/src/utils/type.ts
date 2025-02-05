3a0d310bdcd23a169964232d2c083d89581d36bd
Farkas <f.bence8100@gmail.com>
2025-02-05 20:07:38 +0100

    interfaces, frontend logic rewrite, tags and languages are in dev

--- apps/frontend/src/utils/type.ts
+++ apps/frontend/src/utils/type.ts
@@ -1,36 +1,46 @@
-export interface Question {
-  text: string
-  type: '2 válaszos' | 'Normál'
-  image: string
-  answers: string[]
-  correctAnswerIndex: number
-}
-
-export interface Quiz {
-  image: string
+export interface quizUpload {
   title: string
-  desc: string
-  category: string
-  questions: Question[]
-}
-
-export interface Card {
-  uuid: string
-  image: string
-  title: string
-  category: string
-  desc: string
-  created_by: string
-  questions: Question[]
-  answerIndex: number
+  description: string
   status: string
+  banner: string
+  languages: string[]
+  tags: string[]
+  cards: {
+    question: string
+    type: string
+    answers: string[]
+    picture: string
+    correct_answer_index: number
+  }[]
+}
+
+export interface quizCardView {
+  status: string
+  id: string
+  created_at: Date
+  updated_at: Date
+  title: string
+  description: string
+  rating: string
+  plays: number
+  banner: string
+  languages: string[]
+  tags: string[]
+  user_id: string
+  cards: {
+    question: string,
+    type: string
+    answers: string[],
+    picture: string,
+    correct_answer_index: number,
+  }[]
 }
 
 export interface FuzzySearchOptionsForCard {
   threshold?: number
   caseSensitive?: boolean
   trimWhitespace?: boolean
-  keys?: keyof Card | (keyof Card)[] | ((item: Card) => string | string[])
+  keys?: keyof quizCardView | (keyof quizCardView)[] | ((item: quizCardView) => string | string[])
 }
 
 export interface FuzzySearchResult<T> {
