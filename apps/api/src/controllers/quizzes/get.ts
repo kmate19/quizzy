88d63b1514f453b7b24d7891ffc8fa5d050c7db1
kmate19 <kissmate019@gmail.com>
2025-02-05 17:38:00 +0100

    fix: added offset to get quizzes and broke out numericstring zod schema

--- apps/api/src/controllers/quizzes/get.ts
+++ apps/api/src/controllers/quizzes/get.ts
@@ -2,13 +2,15 @@ import GLOBALS from "@/config/globals";
 import db from "@/db";
 import { quizzesTable } from "@/db/schemas";
 import checkJwt from "@/middlewares/checkJwt";
+import { numericString } from "@/utils/schemas/zod-schemas";
 import { zValidator } from "@hono/zod-validator";
 import { eq } from "drizzle-orm";
 import { ApiResponse } from "repo";
 import { z } from "zod";
 
-const getHandlers = GLOBALS.CONTROLLER_FACTORY(checkJwt(), zValidator('query', z.object({ limit: z.string().regex(/^\d+$/).transform(Number).optional() })), async (c) => {
+const getHandlers = GLOBALS.CONTROLLER_FACTORY(checkJwt(), zValidator('query', z.object({ limit: numericString.optional(), page: numericString.optional() })), async (c) => {
     const limit = c.req.valid('query').limit || 20;
+    const page = c.req.valid('query').page || 0;
 
     if (limit > 50) {
         const res = {
@@ -22,7 +24,7 @@ const getHandlers = GLOBALS.CONTROLLER_FACTORY(checkJwt(), zValidator('query', z
         return c.json(res, 400);
     }
 
-    const quizzes = await db.select().from(quizzesTable).where(eq(quizzesTable.status, "published")).limit(limit);
+    const quizzes = await db.select().from(quizzesTable).where(eq(quizzesTable.status, "published")).offset(limit * page).limit(limit);
 
     const res = {
         message: "Quizzes fetched",
