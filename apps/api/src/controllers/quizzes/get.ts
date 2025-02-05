30905990155bf562f5662cc34a631e20ced6cb98
kmate19 <kissmate019@gmail.com>
2025-02-05 19:50:51 +0100

    fix: added limit checking inside getting quizzes schema

--- apps/api/src/controllers/quizzes/get.ts
+++ apps/api/src/controllers/quizzes/get.ts
@@ -2,27 +2,15 @@ import GLOBALS from "@/config/globals";
 import db from "@/db";
 import { quizzesTable } from "@/db/schemas";
 import checkJwt from "@/middlewares/checkJwt";
+import { zv } from "@/middlewares/zv";
 import { numericString } from "@/utils/schemas/zod-schemas";
-import { zValidator } from "@hono/zod-validator";
 import { eq } from "drizzle-orm";
 import { ApiResponse } from "repo";
 import { z } from "zod";
 
-const getHandlers = GLOBALS.CONTROLLER_FACTORY(checkJwt(), zValidator('query', z.object({ limit: numericString.optional(), page: numericString.optional() })), async (c) => {
+const getHandlers = GLOBALS.CONTROLLER_FACTORY(checkJwt(), zv('query', z.object({ limit: numericString.refine((num) => num < 51 && num > 9).optional(), page: numericString.optional() })), async (c) => {
     const limit = c.req.valid('query').limit || 20;
     const page = c.req.valid('query').page || 0;
-
-    if (limit > 50) {
-        const res = {
-            message: "Limit cannot exceed 50",
-            error: {
-                message: "Limit cannot exceed 50",
-                case: "bad_request"
-            }
-        } satisfies ApiResponse;
-
-        return c.json(res, 400);
-    }
 
     const quizzes = await db.query.quizzesTable.findMany({
         where: eq(quizzesTable.status, "published"),
