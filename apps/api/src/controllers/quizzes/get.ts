292981fcfae91b826aea241e25285faa3782f5a3
kmate19 <kissmate019@gmail.com>
2025-02-06 16:59:14 +0100

    fix: bugfixes in quiz routes + makeSharpImage helper

--- apps/api/src/controllers/quizzes/get.ts
+++ apps/api/src/controllers/quizzes/get.ts
@@ -10,14 +10,14 @@ import { z } from "zod";
 
 const getHandlers = GLOBALS.CONTROLLER_FACTORY(checkJwt(), zv('query', z.object({ limit: numericString.refine((num) => num < 51 && num > 9).optional(), page: numericString.optional() })), async (c) => {
     const limit = c.req.valid('query').limit || 20;
-    const page = c.req.valid('query').page || 0;
+    const page = c.req.valid('query').page || 1;
 
     const quizzes = await db.query.quizzesTable.findMany({
         where: eq(quizzesTable.status, "published"),
         columns: {
             status: false
         },
-        offset: limit * page,
+        offset: limit * (page - 1),
         limit: limit,
         with: {
             user: {
