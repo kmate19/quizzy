2b3ea74e71491fa3eab5770c0f41abf29c2b32a4
kmate19 <kissmate019@gmail.com>
2025-02-09 11:32:49 +0100

    feat: added get own by id
    ez igy nem pont jo
    
    cdd
    
    s

---
+++ apps/api/src/controllers/quizzes/get-own-by-id.ts
@@ -0,0 +1,51 @@
+import GLOBALS from "@/config/globals";
+import db from "@/db";
+import { quizzesTable } from "@/db/schemas";
+import checkJwt from "@/middlewares/check-jwt";
+import { zv } from "@/middlewares/zv";
+import { and, eq } from "drizzle-orm";
+import { ApiResponse } from "repo";
+import { z } from "zod";
+
+const getOwnByIdHandlers = GLOBALS.CONTROLLER_FACTORY(checkJwt(), zv('param', z.object({ uuid: z.string().uuid() })), async (c) => {
+    const { uuid } = c.req.valid('param')
+    const { userId } = c.get("accessTokenPayload");
+
+    const quiz = await db.query.quizzesTable.findFirst({
+        where: and(eq(quizzesTable.user_id, userId), eq(quizzesTable.id, uuid)),
+        with: {
+            cards: true,
+            tags: {
+                with: {
+                    tag: true
+                }
+            },
+            languages: {
+                with: {
+                    language: true
+                }
+            }
+        }
+    });
+
+    if (!quiz) {
+        const res = {
+            message: "Quiz not found",
+            error: {
+                message: "Quiz not found",
+                case: "not_found"
+            }
+        } satisfies ApiResponse;
+
+        return c.json(res, 404);
+    }
+
+    const res = {
+        message: "Quiz fetched",
+        data: quiz
+    } satisfies ApiResponse;
+
+    return c.json(res, 200);
+});
+
+export default getOwnByIdHandlers;
