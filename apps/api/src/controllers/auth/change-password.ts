44cccc03bcf2bc33b9579ebe235041cf2373cd5e
kmate19 <kissmate019@gmail.com>
2025-02-14 18:17:01 +0100

    fix: igen

--- apps/api/src/controllers/auth/change-password.ts
+++ apps/api/src/controllers/auth/change-password.ts
@@ -5,10 +5,26 @@ import checkJwt from "@/middlewares/check-jwt";
 import { zv } from "@/middlewares/zv";
 import { eq } from "drizzle-orm";
 import type { ApiResponse } from "repo";
+import { z } from "zod";
 
-const changePasswordHandler = GLOBALS.CONTROLLER_FACTORY(checkJwt(), zv('json', LoginUserSchema.omit({ username_or_email: true })), async (c) => {
+const changePasswordHandler = GLOBALS.CONTROLLER_FACTORY(checkJwt(), zv('json', LoginUserSchema.omit({ username_or_email: true }).extend({ oldPassword: z.string() })), async (c) => {
     const { userId } = c.get('accessTokenPayload');
-    const { password } = c.req.valid('json');
+    const { password, oldPassword } = c.req.valid('json');
+
+    const [user] = await db.select().from(usersTable).where(eq(usersTable.id, userId));
+
+    try {
+        await Bun.password.verify(user.password, oldPassword);
+    } catch (e) {
+        const res = {
+            message: 'Invalid old password',
+            error: {
+                message: 'invalid_old_password',
+                case: "unauthorized"
+            }
+        } satisfies ApiResponse;
+        return c.json(res, 401);
+    }
 
     const newPassword = await Bun.password.hash(password);
     await db.update(usersTable).set({ password: newPassword }).where(eq(usersTable.id, userId));
