40b9529f7e76634c813014270f2e525baf251e65
kmate19 <kissmate019@gmail.com>
2025-02-15 20:57:52 +0100

    test: did middleware test todos

--- apps/api/tests/middleware.test.ts
+++ apps/api/tests/middleware.test.ts
@@ -7,6 +7,10 @@ import db from '@/db';
 import * as schema from "@/db/schemas/index";
 import GLOBALS from '@/config/globals';
 import { ApiResponse } from 'repo';
+import { and, eq } from 'drizzle-orm';
+import ENV from '@/config/env';
+import { QuizzyJWTPAYLOAD } from '@/types';
+import { sign, verify } from 'hono/jwt';
 
 const client = testClient(app).api.v1;
 
@@ -29,15 +33,85 @@ describe("tests for middleware", () => {
             expect(json).toHaveProperty('error');
             expect(json?.error?.message).toBe("not logged in");
         });
-        test.todo("it is successful if everything is normal", async () => {
+        test("it is successful if everything is normal", async () => {
+            const { cookies } = await registerAndLogin(client);
+
+            const res = await client.auth.authed.$get({ query: {}, }
+                , { headers: { cookie: cookies.join(';') } });
+
+            expect(res.status).toBe(200);
         });
-        test.todo("it fails if user doesnt have the required role", async () => {
+        test("it fails if user doesnt have the required role", async () => {
+            const { cookies } = await registerAndLogin(client);
+
+            const res = await client.auth.authed.$get({ query: { role: "admin" }, }
+                , { headers: { cookie: cookies.join(';') } });
+
+            const json = await res.json() as ApiResponse;
+            expect(res.status).toBe(403);
+            expect(json).toHaveProperty('error');
+            expect(json?.error?.message).toBe("user does not have the required role");
         });
-        test.todo("it succeeds if user does have the required role", async () => {
+        test("it succeeds if user does have the required role", async () => {
+            const { cookies } = await registerAndLogin(client);
+
+            const [user] = await db.select().from(schema.usersTable).where(eq(schema.usersTable.username, "mateka"));
+            const [adminRole] = await db.select().from(schema.rolesTable).where(eq(schema.rolesTable.name, "admin"));
+            await db.insert(schema.userRolesTable).values({ user_id: user.id, role_id: adminRole.id }).onConflictDoNothing();
+
+            const res = await client.auth.authed.$get({ query: { role: "admin" }, }
+                , { headers: { cookie: cookies.join(';') } });
+
+            expect(res.status).toBe(200);
         });
-        test.todo("it refreshes acccess token if expired", async () => {
+        test("it refreshes acccess token if expired", async () => {
+            const { cookies } = await registerAndLogin(client);
+
+            const jwt = cookies.join(';').split('=')[1].split(';')[0];
+
+            // expire the jwt
+            const decoded = await verify(jwt, ENV.ACCESS_JWT_SECRET()) as QuizzyJWTPAYLOAD;
+
+            decoded.exp = Math.floor(Date.now() / 1000) - 10;
+
+            const newJwt = await sign(decoded, ENV.ACCESS_JWT_SECRET());
+
+            const cookiesModified = [`${GLOBALS.ACCESS_COOKIE_NAME}=${newJwt};`
+                .concat(cookies.join(';').split(';').slice(1).join(';'))];
+
+            const res = await client.auth.authed.$get({ query: {}, }
+                , { headers: { cookie: cookiesModified.join(';') } });
+
+            expect(res.status).toBe(200);
         });
-        test.todo("it fails if refresh token is expired", async () => {
+        test("it fails if refresh token is expired", async () => {
+            const { cookies } = await registerAndLogin(client);
+
+            const [user] = await db.select().from(schema.usersTable).where(eq(schema.usersTable.username, "mateka"));
+            const [userToken] = await db.select().from(schema.userTokensTable).where(and(
+                eq(schema.userTokensTable.user_id, user.id), eq(schema.userTokensTable.token_type, "refresh")));
+
+            const jwt = cookies.join(';').split('=')[1].split(';')[0];
+
+            // expire the jwt
+            const decoded = await verify(jwt, ENV.ACCESS_JWT_SECRET()) as QuizzyJWTPAYLOAD;
+
+            decoded.exp = Math.floor(Date.now() / 1000) - 10;
+
+            const newJwt = await sign(decoded, ENV.ACCESS_JWT_SECRET());
+
+            const cookiesModified = [`${GLOBALS.ACCESS_COOKIE_NAME}=${newJwt};`
+                .concat(cookies.join(';').split(';').slice(1).join(';'))];
+
+            await db.update(schema.userTokensTable).set({ expires_at: new Date() }).where(eq(schema.userTokensTable.id, userToken.id));
+
+            const res = await client.auth.authed.$get({ query: {}, }
+                , { headers: { cookie: cookiesModified.join(';') } });
+
+            const json = await res.json() as ApiResponse;
+            expect(res.status).toBe(401);
+            expect(json).toHaveProperty('error');
+            expect(json?.error?.message).toBe("refresh token expired");
         });
         // TODO:
         test.todo("edge case access token exists in cookies and still not expired but doesnt exist elsewhere anymore (nor the user)", async () => {
