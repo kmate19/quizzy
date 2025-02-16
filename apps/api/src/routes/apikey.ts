021a73f8ac92f5b556c5a9b1e25499f1701f36c6
kmate19 <kissmate019@gmail.com>
2025-02-16 08:57:10 +0100

    docs: described apikey routes, changed zvalidator to autogen openapi spec

--- apps/api/src/routes/apikey.ts
+++ apps/api/src/routes/apikey.ts
@@ -2,10 +2,64 @@ import { Hono } from "hono";
 import createHandler from "@/controllers/apikey/create";
 import deleteHandler from "@/controllers/apikey/delete";
 import listHandler from "@/controllers/apikey/list";
+import { describeRoute } from "hono-openapi";
+import { z } from "zod";
+import { resolver } from "hono-openapi/zod";
+
+console.error(...createHandler)
 
 const apikey = new Hono().basePath("/apikey")
-    .post("/create", ...createHandler)
-    .get("/list", ...listHandler)
-    .delete("/delete/:id", ...deleteHandler)
+    .post("/create",
+        describeRoute({
+            description: "Create an API key",
+            responses: {
+                200: {
+                    description: "Success - apikey created",
+                    content: {
+                        "application/json": {
+                            schema: resolver(z.object({ message: z.string(), data: z.string() })),
+                        },
+                    },
+                },
+            }
+        }), ...createHandler)
+
+    .get("/list",
+        describeRoute({
+            description: "List own API keys",
+            responses: {
+                200: {
+                    description: "Success - list of apikeys (trunctated)",
+                    content: {
+                        "application/json": {
+                            schema: resolver(z.object({
+                                message: z.string(), data: z.object({
+                                    description: z.string().nullable(),
+                                    key: z.string(),
+                                    created_at: z.string(),
+                                    id_by_user: z.number(),
+                                    expires_at: z.string().datetime(),
+                                }).array()
+                            })),
+                        },
+                    },
+                },
+            }
+        }), ...listHandler)
+
+    .delete("/delete/:id",
+        describeRoute({
+            description: "Delete an apikey",
+            responses: {
+                200: {
+                    description: "Success - apikey deleted",
+                    content: {
+                        "application/json": {
+                            schema: resolver(z.object({ message: z.string() })),
+                        },
+                    },
+                },
+            }
+        }), ...deleteHandler)
 
 export default apikey;
--- apps/api/src/routes/meta.ts
+++ apps/api/src/routes/meta.ts
@@ -1,9 +1,28 @@
 import { Hono } from "hono";
 import getLanguagesHandlers from "@/controllers/meta/get-languages";
 import getTagsHandlers from "@/controllers/meta/get-tags";
+import { describeRoute } from "hono-openapi";
+import { resolver } from "hono-openapi/zod";
+import { z } from "zod";
 
 const meta = new Hono().basePath("/meta")
-    .get("/tags", ...getTagsHandlers)
+    .get("/tags",
+        describeRoute({
+            description: "Get all tags",
+            responses: {
+                200: {
+                    description: "Success",
+                    content: {
+                        "application/json": {
+                            schema: resolver(
+                                z.object({ name: z.string() }).array()
+                            )
+                        },
+                    },
+                }
+            }
+        })
+        , ...getTagsHandlers)
     .get("/languages", ...getLanguagesHandlers)
 
 export default meta;
