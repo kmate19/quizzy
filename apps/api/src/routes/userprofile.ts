103462e62f922e5333e28cc3d0560055109c376f
kmate19 <kissmate019@gmail.com>
2025-02-18 18:30:19 +0100

    docs: added single userprofile describe
    asd

--- apps/api/src/routes/userprofile.ts
+++ apps/api/src/routes/userprofile.ts
@@ -1,10 +1,11 @@
 import getBaseDataHandler from "@/controllers/userprofile/get";
 import getProfileByIdHandler from "@/controllers/userprofile/get-by-id";
 import postProfilePictureHandler from "@/controllers/userprofile/post-profile-picture";
+import { getUserprofileDesc } from "@/openapi/userprofile-openapi";
 import { Hono } from "hono";
 const userprofile = new Hono()
     .basePath("/userprofile")
-    .get("/", ...getBaseDataHandler)
+    .get("/", getUserprofileDesc, ...getBaseDataHandler)
     .post("/profilepic", ...postProfilePictureHandler)
     .get("/:userId", ...getProfileByIdHandler);
 
