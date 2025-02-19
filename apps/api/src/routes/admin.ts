0bec799e719147b479a9f6d1553397d85827d97e
kmate19 <kissmate019@gmail.com>
2025-02-19 10:26:55 +0100

    feat: added authenticate route for apikey

--- apps/api/src/routes/admin.ts
+++ apps/api/src/routes/admin.ts
@@ -1,10 +1,12 @@
 import setAuthStatusHandlers from "@/controllers/admin/set-auth-status";
 import setRoleHandlers from "@/controllers/admin/set-role";
+import check_apikey from "@/middlewares/check-apikey";
 import { Hono } from "hono/tiny";
 
 const admin = new Hono()
     .basePath("/admin")
     .post("/set/role", ...setRoleHandlers)
-    .post("/set/authstatus", ...setAuthStatusHandlers);
+    .post("/set/authstatus", ...setAuthStatusHandlers)
+    .get("/authenticate", check_apikey);
 
 export default admin;
