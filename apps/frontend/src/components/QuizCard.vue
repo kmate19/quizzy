b043765a0097447295e081070e5cb9aac83f6d71
Farkas Bence Zsolt <farkas.b@ckik.loc>
2025-03-03 09:13:44 +0100

    emoji

--- apps/frontend/src/components/QuizCard.vue
+++ apps/frontend/src/components/QuizCard.vue
@@ -31,8 +31,7 @@ defineProps<{ quiz: quizCardView }>()
         <div class="flex flex-wrap gap-2 mb-2">
           <span v-for="lang in quiz.languages" :key="lang.iso_code"
             class="inline-flex items-center bg-blue-500 bg-opacity-50 rounded-full px-2 py-1 text-xs text-white">
-            <img :src="lang.iso_code" :alt="lang.iso_code" class="w-4 h-4 mr-1" />
-            {{ lang.name }}
+            {{ lang.icon }} {{ lang.name }} 
           </span>
         </div>
         <div class="flex flex-wrap gap-2 max-h-20 h-fit overflow-y-scroll custom-scrollbar">
