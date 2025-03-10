10bb03a49ef5c8636725ee4d2656598ade22b62e
Farkas <f.bence8100@gmail.com>
2025-03-10 19:06:04 +0100

    navbar fix

--- apps/frontend/src/App.vue
+++ apps/frontend/src/App.vue
@@ -2,11 +2,22 @@
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools'
 import MistBackground from './components/MistBackground.vue'
 import NavBar from './components/NavBar.vue';
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+const route = useRoute();
+const hide = ['/login', '/forgotPw'];
+const isNavbar = computed(() =>{
+  console.log("Route fullPath:", route.fullPath, "Show NavBar:", isNavbar.value);
+  return !hide.includes(route.fullPath as string);
+}) 
+
+
 </script>
 
 <template>
   <VueQueryDevtools />
-  <NavBar/>
-  <MistBackground/>
-  <RouterView/>
+  <NavBar v-if="isNavbar"/>
+  <MistBackground />
+  <RouterView :key="route.fullPath" />
 </template>
