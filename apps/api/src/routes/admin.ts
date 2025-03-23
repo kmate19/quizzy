13048422fabcaaba767e3603dfad918abcc718d1
kmate19 <kissmate019@gmail.com>
2025-03-23 20:17:59 +0100

    feat: added routes

--- apps/api/src/routes/admin.ts
+++ apps/api/src/routes/admin.ts
@@ -1,5 +1,7 @@
+import getAllQuizHandlers from "@/controllers/admin/get-all-quiz";
 import getAllUsersHandlers from "@/controllers/admin/get-all-users";
 import setAuthStatusHandlers from "@/controllers/admin/set-auth-status";
+import setQuizHandlers from "@/controllers/admin/set-quiz-status";
 import setRoleHandlers from "@/controllers/admin/set-role";
 import check_apikey from "@/middlewares/check-apikey";
 import { Hono } from "hono/tiny";
@@ -8,6 +10,8 @@ const admin = new Hono()
     .basePath("/admin")
     .post("/set/role", ...setRoleHandlers)
     .post("/set/authstatus", ...setAuthStatusHandlers)
+    .post("/set/quiz", ...setQuizHandlers)
+    .post("/all-quizzes", ...getAllQuizHandlers)
     .get("/all-users", ...getAllUsersHandlers)
     .get("/authenticate", async (c) => {
         // @ts-expect-error not passing next
