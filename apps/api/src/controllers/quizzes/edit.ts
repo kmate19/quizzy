e4048d8ad7672e7f03639bf07d10d353d8eefe10
kmate19 <kissmate019@gmail.com>
2025-04-09 14:05:46 +0200

    feat: added ratelimit to quiz creation

--- apps/api/src/controllers/quizzes/edit.ts
+++ apps/api/src/controllers/quizzes/edit.ts
@@ -15,11 +15,20 @@ import { processImage } from "@/utils/helpers";
 import { editQuizSchema } from "@/utils/schemas/zod-schemas";
 import { and, eq } from "drizzle-orm";
 import { fileTypeFromBuffer } from "file-type";
+import { rateLimiter } from "hono-rate-limiter";
+import { getConnInfo } from "hono/bun";
 import { ApiResponse } from "repo";
 import { z } from "zod";
 
 const editHandlers = GLOBALS.CONTROLLER_FACTORY(
     checkJwt(),
+    rateLimiter({
+        windowMs: 2 * 60 * 1000,
+        limit: 2,
+        standardHeaders: "draft-7",
+        keyGenerator: (c) => getConnInfo(c).remote.address!,
+        message: "Too many requests, please try again later.",
+    }),
     zv("json", editQuizSchema),
     zv("param", z.object({ quizId: z.string().uuid() })),
     async (c) => {
